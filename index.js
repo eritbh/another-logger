93794fa612eb8e6bfcde08a1a5e486767d52d501
George <georgej1088@gmail.com>
2018-07-04 18:47:15 -0400

    Restructure config, correct doc comments, make styleFrom safe

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const util = require('util')
 const chalk = require('chalk')
 const path = require('path')
 
+// Default config options
+const defaultConfig = {
+	timestamps: false,
+	levels: {
+		debug: {style: 'cyan'},
+		info: {style: 'blue'},
+		success: {style: 'green'},
+		warn: {text: 'warning', style: 'yellow'},
+		error: {style: 'red'},
+		log: {text: 'info', style: 'blue'}
+	},
+	ignoredLevels: [],
+	label: '',
+	labelStyle: null
+}
+
 // Attempt to read config from process.cwd
 let baseConfig
 try {
@@ -12,19 +28,12 @@ try {
 } catch (_) {
 	baseConfig = {}
 }
+baseConfig = Object.assign({}, defaultConfig, baseConfig)
+baseConfig.levels = Object.assign({}, defaultConfig.levels, baseConfig.levels)
 
 function normalize (_, ...subs) {
 	return subs.filter(s => s).join(' ')
-}
-
-const defaultLevels = {
-	debug: {style: 'cyan'},
-	info: {style: 'blue'},
-	success: {style: 'green'},
-	warn: {text: 'warning', style: 'yellow'},
-	error: {style: 'red'},
-	log: {text: 'info', style: 'blue'}
-}
+}	
 
 /**
  * Converts a string representation of a style to a styling function.
@@ -32,15 +41,18 @@ const defaultLevels = {
  */
 function styleFrom (style) {
 	// If this isn't a string, return it as it is
-	if (typeof style !== 'string') {
+	if (typeof style === 'function') {
 		return style
+	} else if (typeof style === 'string') {
+		const parts = style.split(/[. ]/g).filter(s => s)
+		style = chalk
+		for (let part of parts) {
+			style = style[part]
+		}
+		return style
+	} else {
+		return s => s
 	}
-	const parts = style.split(/[. ]/g).filter(s => s)
-	style = chalk
-	for (let part of parts) {
-		style = style[part]
-	}
-	return style
 }
 
 /**
@@ -53,16 +65,12 @@ class Logger {
 	 * @param {object} [options = {}] - An object of options for the logger.
 	 * @param {boolean} [options.timestamps = false] - Whether or not log output
 	 * should contain timestamps.
-	 * @param {number} [options.minLevel = 1] - The minimum log level to be
-	 * displayed.
-	 * @param {number} [options.maxLevel = Infinity] - The maximum log level to be
-	 * displayed.
 	 * @param {object} [options.levels = {}] - An object of levels to use in
 	 * addition to the default ones.
 	 * @param {string[]} [options.ignoredLevels = []] - An array of level names
 	 * which shouldn't be logged.
 	 * @param {string} [options.label] - Same as the `label` argument. If both are
-	 * defined, this one takes precedence.
+	 * defined, the other argument takes precedence.
 	 * @param {function|string} [options.labelStyle] - A style to apply to the
 	 * label. If this is a string, it corresponds to a name supported by `chalk`.
 	 * If it is a function, the result of passing the label through the function
@@ -75,50 +83,40 @@ class Logger {
 			_label = undefined
 		}
 
-		// Add config to base if we have a base and apply label to config
-		config = Object.assign(baseConfig, config, {label: _label})
-
-		const {
-			timestamps = false,
-			levels = {},
-			ignoredLevels = [],
-			label = '',
-			labelStyle = chalk.gray.bold
-		} = config
+		// Apply given label and configuration to base config
+		config = Object.assign(baseConfig, config, _label ? {label: _label} : {})
 
 		/**
 		 * @prop {object} timestamps Whether or not log output should comtain timestamps.
 		 */
-		this.timestamps = timestamps
+		this.timestamps = config.timestamps
 
 		/**
 		 * @prop {object} levels All levels in use in this logger.
 		 */
-		this.levels = Object.assign(defaultLevels, levels)
+		this.levels = Object.assign({}, defaultConfig.levels, config.levels)
 
 		/**
 		 * @prop {string[]} ignoredLevels Levels which shouldn't be logged.
 		 */
-		this.ignoredLevels = ignoredLevels
+		this.ignoredLevels = config.ignoredLevels
 
 		/**
 		 * @prop {string} label A label to print with each log line.
 		 */
 		// In order to avoid generating formattedLabel twice, we set the private
 		// version (_label) here. The generation happens when labelStyle is set.
-		this._label = label
+		this._label = config.label
 
 		/**
 		 * @prop {function} labelStyle A style function to add to the label.
 		 */
-		this.labelStyle = styleFrom(labelStyle)
+		this.labelStyle = styleFrom(config.labelStyle)
 
 		// Dynamically create functions for each of the levels.
 		Object.keys(this.levels).forEach(name => {
 			if (this[name]) throw new TypeError('Invalid level name', name)
-			this[name] = function (...contents) {
-				this._log(name, ...contents)
-			}
+			this[name] = this._log.bind(this, name)
 		})
 	}
 
@@ -155,10 +153,10 @@ class Logger {
 	 * @param {...*} contents - The contents of the log.
 	 */
 	_log (name, ...contents) {
-		let {text, style, level} = this.levels[name]
+		let {text, style} = this.levels[name]
 		if (this.ignoredLevels.includes(name)) return
 		name = text || name
-		style = styleFrom(style) || (s => s)
+		style = styleFrom(style)
 		const timestamp = this.timestamps ? this._getTimestamp() : ''
 		name = style(name)
 		console.log(normalize`${timestamp} ${this.formattedLabel} ${name} ${util.format(...contents)}`)
@@ -166,5 +164,5 @@ class Logger {
 }
 
 module.exports = Logger
-module.exports.defaultLevels = defaultLevels
+module.exports.defaultConfig = defaultConfig
 module.exports.styleFrom = styleFrom
