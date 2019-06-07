0895fac068a6688c09cd41669f6462d5c24dc4ac
George <georgej1088@gmail.com>
2019-06-07 04:03:07 -0400

    Update typings

--- index.d.ts
+++ index.d.ts
@@ -1,39 +1,52 @@
 // Defines the logger object.
 interface Logger {
-    debug: LoggingFunction;
-    info: LoggingFunction;
-    log: LoggingFunction;
-    success: LoggingFunction;
-    warn: LoggingFunction;
-    error: LoggingFunction;
     [level: string]: LoggingFunction;
 }
 
 // Defines a function that logs things.
 interface LoggingFunction {
-    // log.info()
+    /** Send a message through the logger. */
     (...contents: any[]): void;
-    // log.info.trace()
+    /** Send a message and include a stack trace. */
     trace (...contents: any[]): void;
-    // log.info.table()
+    /**
+     * Render a table with the given input, optionally filtering the rendered
+     * properties.
+     */
     table (tabularData: any, properties?: string[]): void;
 }
 
 // Defines config parameters for new loggers.
 interface Config {
+    /** Whether or not messages should display timestamps */
     timestamps?: boolean;
+    /** A string to display with all messages this logger sends */
     label?: string;
+    /** An array or object of ignored level names */
     ignoredLevels?: string[] | {
         [x: string]: boolean;
     };
+    /** An object of custom levels to add to the logger */
     levels?: Levels;
+    /** A stream that receives all output from this logger */
+    stream?: CustomStream;
 }
 
-// Defines log levels for new loggers.
+/** An object that can serve as a custom output stream. */
+interface CustomStream {
+    write (data: string): any;
+}
+
+/** An object defining additional log levels. */
 interface Levels {
+    /** The name of the level */
     [level: string]: {
+        /** The text displayed when the level is used */
         text?: string;
+        /** A string or function used to style the level's text */
         style?: string | ((string) => string);
+        /** A stream that receives all output from this level */
+        stream: CustomStream;
     };
 }
 
@@ -41,8 +54,9 @@ interface Levels {
 // itself. If you don't need to pass additional config as arguments, you can
 // just require the module and start using it without creating another instance.
 interface LoggerConstructor extends Logger {
-    (config?: Config, levels?: Levels): Logger;
+    (config?: Config): Logger;
 }
 
+/** The default logger object. */
 declare const logger: LoggerConstructor;
 export = logger;
