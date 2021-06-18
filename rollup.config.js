ed4990b1da493cc8d4afc5a90226b2a473dd850a
Erin <erin20913@gmail.com>
2021-06-18 18:07:07 -0400

    Automatically set external modules for Rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,17 @@
 import typescript from '@rollup/plugin-typescript';
+import {builtinModules} from 'module';
+import packageJson from './package.json';
 
 export default [
 	{
 		preserveModules: true,
 		input: 'src/index.ts',
+		external: [
+			// Modules provided by Node
+			...builtinModules,
+			// Dependencies from npm
+			...Object.keys(packageJson.dependencies),
+		],
 		output: [
 			{
 				dir: 'dist',
